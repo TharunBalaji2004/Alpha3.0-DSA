f073ca06a412f600ac16e4012b211c60c48748fe
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-01-18 12:47:04 +0530

    PQ2 Inverted & Rotated Half Pyramid

---
+++ L5 Patterns Part 1/pq2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class pq2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of lines: ");
+        int lines = sc.nextInt();
+
+        for(int i=1;i<=lines;i++){
+            for(int gap=1;gap<=(lines-i);gap++){
+                System.out.print(" ");
+            }
+            for(int star=1;star<=i;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+OUTPUT:
+
+Enter number of lines: 7
+      *
+     **
+    ***
+   ****
+  *****
+ ******
+*******
+    
+*/
