ac8fc21aa20981b637f2cdf474dd8ecd98d23c9a
Tharun Balaji R <95350584+TharunBalaji2004@users.noreply.github.com>
2023-01-18 12:37:14 +0530

    PQ1 Hollow Rectangle

---
+++ L5 Patterns Part 1/pq1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class pq1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter length: ");
+        int length = sc.nextInt();
+        System.out.print("Enter breadth: ");
+        int breadth = sc.nextInt();
+
+        for(int i=1;i<=breadth;i++){
+            System.out.print("* ");
+            for(int j=1;j<=length;j++){         
+                if (i == 1 || i == breadth){
+                    System.out.print("* ");
+                }
+                else {
+                    System.out.print("  ");
+                }            
+            }
+            System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
+
+/*
+OUTPUT: 
+Enter length: 5
+Enter breadth: 4
+ 
+* * * * *
+*       *
+*       *
+* * * * *
+
+length = Number of columns
+breadth = Number of rows
+*/
